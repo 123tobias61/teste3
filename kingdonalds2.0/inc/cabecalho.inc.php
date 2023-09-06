08fe76ab40d7a262b852e8e9fa9b1281bad80699
123tobias61 <144290896+123tobias61@users.noreply.github.com>
2023-09-06 20:56:08 -0300

    Add files via upload

---
+++ kingdonalds2.0/inc/cabecalho.inc.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><kingdonalds2 class="0"></kingdonalds2></title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+
+
+    <header>
+    <a href="index.php"><img src="img/Meu projeto.png" alt="logo king donald's"></a>
+    
+        <!--- inicio sessao de busca-->
+       
+            
+            <!--- fim da sessao-->
+            <section class="busca">  
+
+            <h2> <strong>pesquisar </strong></h2>
+            <form> 
+                <input type="search"
+                placeholder="informe o iten">
+                <button>buscar</button>
+            </form>
+
+        </section> 
+        <nav class="menu_opcoes">
+<ul> <strong>
+        <li><a href="#">sua conta </a></li>
+        <li><a href="#"> combos </a></li>
+        <li><a href="#">cardapio </a></li>
+        <li><a href="#">ajuda </a></li>
+        <li><a href="sobre.php">sobre </a></li></strong>
+</ul>
+</nav>
+
+ 
+</header>
---
+++ kingdonalds2.0/inc/rodape.inc.php
@@ -0,0 +1,16 @@
+<footer>
+    <img src="img/Meu projeto.png" alt="logo king donald's">
+
+ <ul class="social">
+ 
+<li><a href="http://localhost/responsivel/guia.php">facebook</a></li>
+<li><a href="https://www.twiter.com/">twiter</a></li>
+<li><a href="https://www.instagram.com/">instagran</a></li>
+<li><a href="https://www.youtube.com/">discord</a></li>
+ 
+</ul>
+    <hr>
+    &COPY;king donalds todos os direitos reservados
+   </footer>
+    </body>
+</html>
